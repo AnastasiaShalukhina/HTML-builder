8c797cd51aba833f18e246f25dc6d880a9708e53
AnastasiaShalukhina <exachka4@gmail.com>
2022-11-01 16:13:43 +0300

    feat: add solution for task1

---
+++ 01-read-file/index.js
@@ -0,0 +1,19 @@
+async function readData() {
+  try {
+    const fs = require('fs');
+    const path = require('path');
+    const streamRead = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+    let data = '';
+    streamRead.on('data', chunk => { data += chunk } );
+    streamRead.on('end', () => {
+    console.log(data);
+});
+  }
+  catch(error) {
+    console.log('error');
+  }
+}
+readData()
+
+
+
