026e669f20935576f87700cdb8753cc42fe972f8
AnastasiaShalukhina <exachka4@gmail.com>
2022-11-02 17:43:11 +0300

    add solution for second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+async function keepTalking() {
+  try {
+    const fs = require('fs');
+    const path = require('path');
+    const { stdin: input, stdout: output, stdout } = require('process'); 
+    const readline = require('readline');
+    const write = fs.createWriteStream(path.join(__dirname, 'destination.txt'));
+    const rl = readline.createInterface({ input, output });
+    process.on('exit', () => {
+        console.log("Удачи в кросс-чеке!");
+    });
+    stdout.write('Тяженько в RS учится? ')
+
+    rl.on('line', answer => {
+      
+      if(answer.trim() !== "exit") {
+        write.write(answer + "\n");
+      } else {
+        rl.close();
+      }
+           
+    });
+  }  
+  catch(error) {
+    console.log('error');
+  }
+}
+keepTalking()
